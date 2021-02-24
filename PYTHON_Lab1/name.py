a5d536379e15bad77450cb8164422406bf75538b
bijay <bijay.gautam1501@gmail.com>
2021-02-24 11:47:50 +0545

    Area,,,

---
+++ PYTHON_Lab1/name.py
@@ -0,0 +1,12 @@
+#WAP to enter name and age.
+
+userName = str(input("enter your name: "))
+userAge = int(input(" enter your age: "))
+print(f"My name is {userName} and my age is {userAge}.")
+print("My name is", userName, " and my age is", userAge, ".")
+print("My name is " + userName + "and my age is" + str(userAge)+ ".")
+
+userinfo = [userName, userAge]
+
+print("My name is",userinfo[0], "and my age is", userinfo[1], ".")
+print ("My name is {} and my age is {}." .format(userName, userAge))
