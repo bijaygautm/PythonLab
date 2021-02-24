ef7909453f3dca7bd3b8937e68ad65467077fc6d
bijay.gautam1501@gmail.com <bijay@Bijays-MacBook-Air.local>
2021-02-24 11:16:56 +0545

    Initial commit

---
+++ PYTHON_Lab1/Addn.py
@@ -0,0 +1,8 @@
+#Write a program that takes three numbers and print their sum. Every number is guiven on separate line.
+
+val1 = float(input("give the first number: \n"))
+val2 = float(input(" give the second number: \n"))
+val3 = float(input("give the second number: \n"))
+
+sumTotal = val1 + val2 + val3
+print(sumTotal)
