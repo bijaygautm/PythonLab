8e71dc1ea400b8d1e00a208e6324b078ef1db62f
bijay.gautam1501@gmail.com <bijay@Bijays-MacBook-Air.local>
2021-02-24 11:27:10 +0545

    first commit

---
+++ PYTHON_Lab1/Areatrn.py
@@ -0,0 +1,6 @@
+# Write a program that reads the length of the base and the height of a right-angled triangle and print the area.
+
+base1 = float(input("enter the base: \n"))
+height2 = float(input("enter the height: \n"))
+areaTri = 1/2 * ( base1 * height2)
+print(areaTri)
